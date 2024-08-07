1603451910db53366eba2f68f5d8426fb35c5728
Rafael Pérez <rafaelsaxo12@gmail.com>
2024-08-06 21:53:20 -0400

    'add models of actor,genre and director'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const routerGenre = require('./genre.router');
+const routerActor = require('./actor.router');
+const routerDirector = require('./director.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('/genres', routerGenre)
+router.use('/actors', routerActor)
+router.use('/directors', routerDirector)
 
 module.exports = router;
